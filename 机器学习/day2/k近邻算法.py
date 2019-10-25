e2d16dcab0bf6f4e9e1e0fdde4b44ddf8201a5c3
houyao <2428437133@qq.com>
2019-10-25 09:26:17 +0800

    学习了k近邻算法的使用

---
+++ 机器学习/day2/k近邻算法.py
@@ -0,0 +1,61 @@
+from sklearn.neighbors import KNeighborsClassifier  # k近邻算法API
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+
+
+def k_api():
+	# n_neighbors 查询默认使用的邻居数
+	# algorithm
+	kn = KNeighborsClassifier(n_neighbors=5)
+	# 读取数据
+	data = pd.read_csv('./???.csv')  # csv文件是酒店得到的信息，目前没有
+	print(data.head(10))
+	# 处理数据
+	# 1.缩小数据，查询数据筛选
+	data = data.query('x > 1.0 & x < 1.25')
+
+	# 2.处理时间
+	time_value = pd.to_datetime(data['time'], unit='s')
+
+	# 把日期格式转换成字典格式
+	time_value = pd.DatetimeIndex(time_value)
+	# 构造特征
+	data['day'] = time_value.day
+	data['hour'] = time_value.hour
+	data['weekday'] = time_value.weekday
+
+	# 把时间戳特征删除
+	data.drop(['time'], axis=1)  # axis=1，按列删除
+	print(data)
+
+	# 把签到数量少于n个目标位置删除
+	place_count = data.groupby('place_id').count()  # 此时的index以悄然变成了place_id  且此时place_count会保留出place_id以外的特征，数值都为次数
+	# place_count 是分组之后数据， row_id 变成了具体的次数
+	tf = place_count[place_count.row_id > 3].reset_index()  # reset_index作用是使place_id变为数据的一个特征
+	data = data[data['place_id'].isin(tf.place_id)]  # 筛选出data数据当中 place_id 相等的数据
+
+	# 取出数据当中的特征值和目标值
+	y = data['place_id']
+	x = data.drop(['place_id'], axis=1)  # 按列删除
+	# 进行数据的分割训练集和测试集
+	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)  # 测试集数据占test_size 25%
+
+	# 特征工程（标准化）
+	std = StandardScaler()
+	# 对测试集和训练集的特征值做标准化
+	x_train = std.fit_transform(x_train)
+	x_test = std.transform(x_test)
+
+
+	# 进行算法流程  knn估计器流程
+	knn = KNeighborsClassifier(n_neighbors=5)
+	knn.fit(x_train, y_train)
+	# 得出预测结果
+	y_predict = knn.predict(x_test)
+	print(f'预测的目标签到位置为：{y_predict}')
+	# 得出准确率
+	print(f'预测的准确率：{knn.score(x_test, y_test)}')
+
+
+
--- 机器学习/day2/数据的划分.py
+++ 机器学习/day2/数据的划分.py
@@ -1,4 +1,5 @@
-from sklearn.datasets import load_iris  # sklearn提供的一个案例
+from sklearn.datasets import load_iris, fetch_20newsgroups, load_boston  # sklearn提供的一个案例
+from sklearn.model_selection import train_test_split
 # 以下是用于分类的估计器
 from sklearn.neighbors import *  # k邻近算法
 from sklearn.naive_bayes import *  # 贝叶斯算法
@@ -10,13 +11,33 @@ from sklearn.linear_model import Ridge  # 岭回归
 
 li = load_iris()
 
+# print('获取特征值')
+# print(li.data)
+#
+# print('目标值')
+# print(li.target)
+#
+# print(li.DESCR)  # 获取说明
+# print(li.target_names)
+
+# 返回值，训练集 train   x_train  y_train   测试集 test   x_test  y_test
+# x_train, x_test, y_train, y_test = train_test_split(li.data, li.target, test_size=0.25)  # 测试集数据占test_size 25%
+# print(f'训练集特征值：{x_train}\n训练集特征值目标值：{y_train}')
+# print(f'测试集特征值：{x_test}\n测试集特征值目标值：{y_test}')
+
+news = fetch_20newsgroups(subset='all')
+print(news.data)
+print(news.target)
+
+lb = load_boston()
 print('获取特征值')
-print(li.data)
+print(lb.data)
 
 print('目标值')
-print(li.target)
+print(lb.target)
 
-print(li.DESCR)  # 获取说明
-print(li.target_names)
+print(lb.DESCR)  # 获取说明
+
+
 
 
