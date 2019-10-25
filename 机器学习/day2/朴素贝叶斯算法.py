1ad4731c7bb82c302376d8803cf8ce0141f8588f
houyao <2428437133@qq.com>
2019-10-25 15:46:32 +0800

    学习了朴素贝叶斯算法

---
+++ 机器学习/day2/朴素贝叶斯算法.py
@@ -0,0 +1,36 @@
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB  # 朴素贝叶斯算法api
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
+
+
+def naviebayes():
+	"""朴素贝叶斯进行文本分类"""
+	news = fetch_20newsgroups(subset='all')
+
+	# 数据分割
+	x_train, x_test, y_train, y_test = train_test_split(news.data, news.target, test_size=0.25)
+
+	# 对数据集进行特征抽取
+	tf = TfidfVectorizer()
+
+	# 以训练集当中词的列表进行每篇文章重要性统计
+	x_train = tf.fit_transform(x_train)
+	# print(tf.get_feature_names())
+	x_test = tf.transform(x_test)
+	# 进行朴素贝叶斯算法的预测
+	mlt = MultinomialNB(alpha=1.0)
+	# print(x_train)
+
+	mlt.fit(x_train, y_train)
+	y_predict = mlt.predict(x_test)
+	# 得出准确率
+	print(f'预测的准确率为：{mlt.score(x_test, y_test)}')
+
+
+
+if __name__ == '__main__':
+	naviebayes()
+
+
