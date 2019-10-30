5c09082959656109cc42e75f04b2cfc0d0d2a396
houyao <2428437133@qq.com>
2019-10-30 15:27:53 +0800

    学习了使用均方误差

--- 机器学习/day3/回归.py
+++ 机器学习/day3/回归.py
@@ -12,6 +12,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression, SGDRegressor
 from sklearn.datasets import load_boston
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error
 
 
 def my_linear():
@@ -36,22 +37,24 @@ def my_linear():
 
 	# 预测数据正确性
 	# 正规方程求解预测结果
-	# lr = LinearRegression()
-	# lr.fit(x_train, y_train)
-	# print(f'13个特征值的回归系数：{lr.coef_}')
-	# # 预测房子的价格
-	# y_predict = lr.predict(x_test)
-	# y_predict = std_y.inverse_transform(y_predict)  # 还原真实数据
-	# print(f'测试集里面每个房子的预测价格：{y_predict}')
+	lr = LinearRegression()
+	lr.fit(x_train, y_train)
+	print(f'正规方程13个特征值的回归系数：{lr.coef_}')
+	# 预测房子的价格
+	y_lr_predict = lr.predict(x_test)
+	y_lr_predict = std_y.inverse_transform(y_lr_predict)  # 还原真实数据
+	print(f'正规方程测试集里面每个房子的预测价格：{y_lr_predict}')
+	print(f'正规方程的均方误差：{mean_squared_error(std_y.inverse_transform(y_test), y_lr_predict)}')
 
 	# 梯度下降进行房价预测
 	sgd = SGDRegressor()
 	sgd.fit(x_train, y_train)
-	print(f'13个特征值的回归系数：{sgd.coef_}')
+	print(f'梯度下降13个特征值的回归系数：{sgd.coef_}')
 	# 预测房子的价格
-	y_predict = sgd.predict(x_test)
-	y_predict = std_y.inverse_transform(y_predict)  # 还原真实数据
-	print(f'测试集里面每个房子的预测价格：{y_predict}')
+	y_sgd_predict = sgd.predict(x_test)
+	y_sgd_predict = std_y.inverse_transform(y_sgd_predict)  # 还原真实数据
+	print(f'梯度下降测试集里面每个房子的预测价格：{y_sgd_predict}')
+	print(f'梯度下降的均方误差：{mean_squared_error(std_y.inverse_transform(y_test), y_sgd_predict)}')
 
 
 if __name__ == '__main__':
