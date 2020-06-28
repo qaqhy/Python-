9ee292fbb3e7398b82e74db2741a99e80ed83536
houyao <2428437133@qq.com>
2020-06-28 20:07:26 +0800

    完成了models模型的编写

--- EducationHeaven/edu/models.py
+++ EducationHeaven/edu/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
+
 # Create your models here.
+class Admin(models.Model):
+    aid = models.AutoField()  # 管理员id
+    nickname = models.CharField(max_length=8, unique=True)  # 管理员昵称长度限制为8位
+    position = models.CharField(max_length=8, blank=True)  # 管理员职位
+    phone = models.CharField(max_length=11, unique=True)  # 联系电话
+    mail = models.EmailField()  # 管理员邮箱
+    password = models.CharField(max_length=16)  # 管理员密码
+
+
+class Developer(models.Model):
+    did = models.AutoField()  # 开发者id
+    name = models.CharField(max_length=8)  # 开发者名字
+    phone = models.CharField(max_length=11, unique=True)  # 联系电话
+    mail = models.EmailField()  # 管理员邮箱
+    password = models.CharField(max_length=16)  # 密码
+
+
+class Channel(models.Model):
+    """公司、培训机构、学校、个人工作室"""
+    cid = models.AutoField()  # 渠道id
+    name = models.CharField(max_length=8, unique=True)  # 公司名
+    phone = models.CharField(max_length=11)  # 公司电话
+    mail = models.EmailField()  # 公司邮箱
+    password = models.CharField(max_length=16)  # 密码
+    address = models.CharField(max_length=16)  # 公司地址
+    rmb = models.IntegerField()  # 公司人民币余额
+    eb = models.IntegerField()  # 公司E币余额
+
+
+class Resource(models.Model):
+    """公司、培训机构、学校、个人工作室"""
+    rid = models.AutoField()  # 资源id
+    cid = models.ForeignKey('Channel', on_delete=models.CASCADE)  # 渠道id
+    aid = models.ForeignKey('Admin', on_delete=models.CASCADE)  # 管理员id
+    jurisdiction = models.CharField(max_length=8)  # 权限
+    description = models.CharField(max_length=256)  # 资源说明
+    address = models.CharField(max_length=128)  # 资源地址
+    price = models.CharField(max_length=16, default='0E')  # 价格
+    title = models.CharField(max_length=16)  # 标题
+    enter_date = models.DateField()  # 录入日期
+    pass_date = models.DateField()  # 通过日期
+    view_count = models.IntegerField()  # 观看量
+    password = models.CharField(max_length=16)  # 密码
---
+++ dailyfresh/apps/order/models.py
@@ -0,0 +1,74 @@
+from db.base_model import BaseModel
+from django.db import models
+
+
+# Create your models here.
+class OrderInfo(BaseModel):
+    """订单信息模型类"""
+    PAY_METHOD_CHOICES = (
+        (1, '货到付款'),
+        (2, '微信支付'),
+        (3, '支付宝'),
+        (4, '银联支付'),
+    )
+    ORDER_STATUS_CHOICES = (
+        (1, '待支付'),
+        (2, '待发货'),
+        (3, '待收货'),
+        (4, '待评价'),
+        (5, '已完成'),
+    )
+
+    order_id = models.CharField(max_length=128, primary_key=True, verbose_name='订单编号')
+    user = models.ForeignKey('user.User', verbose_name='用户', on_delete=models.CASCADE)
+    addr = models.ForeignKey('user.Address', verbose_name='地址', on_delete=models.CASCADE)
+    pay_method = models.SmallIntegerField(choices=PAY_METHOD_CHOICES, default=3, verbose_name='购买方式')
+    total_count = models.IntegerField(default=1, verbose_name='商品数量')
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='商品总价')
+    transit_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='运费')
+    order_status = models.SmallIntegerField(choices=ORDER_STATUS_CHOICES, verbose_name='订单状态')
+    trade_no = models.CharField(max_length=128, verbose_name='支付编号')
+
+    class Meta:
+        db_table = 'df_order_info'
+        verbose_name = '订单信息'
+        verbose_name_plural = verbose_name
+
+
+class OrderGoods(BaseModel):
+    """订单商品模块类"""
+    order = models.ForeignKey('OrderInfo', verbose_name='订单信息', on_delete=models.CASCADE)
+    sku = models.ForeignKey('goods.GoodsSKU', verbose_name='商品SKU', on_delete=models.CASCADE)
+    count = models.IntegerField(default=1, verbose_name='商品数目')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='商品价格')
+    phone = models.CharField(max_length=11, verbose_name='联系电话')
+    is_default = models.BooleanField(default=False, verbose_name='是否默认')
+
+    class Meta:
+        db_table = 'df_address'
+        verbose_name = '地址'
+        verbose_name_plural = verbose_name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
